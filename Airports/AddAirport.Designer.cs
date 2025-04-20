2e2c729860e38147d056fc6f2e77cf53287afd91
Sandra Chavdarova <148618364+sandra-chavdarova@users.noreply.github.com>
2025-04-20 15:33:40 +0200

    Add files via upload

---
+++ Airports/AddAirport.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class AddAirport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbCity = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbCode = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbCity
+            // 
+            this.tbCity.Location = new System.Drawing.Point(37, 90);
+            this.tbCity.Name = "tbCity";
+            this.tbCity.Size = new System.Drawing.Size(322, 22);
+            this.tbCity.TabIndex = 0;
+            this.tbCity.Validating += new System.ComponentModel.CancelEventHandler(this.tbCity_Validating);
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(37, 156);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(322, 22);
+            this.tbName.TabIndex = 1;
+            this.tbName.Validating += new System.ComponentModel.CancelEventHandler(this.tbName_Validating);
+            // 
+            // tbCode
+            // 
+            this.tbCode.Location = new System.Drawing.Point(37, 246);
+            this.tbCode.Name = "tbCode";
+            this.tbCode.Size = new System.Drawing.Size(89, 22);
+            this.tbCode.TabIndex = 2;
+            this.tbCode.Validating += new System.ComponentModel.CancelEventHandler(this.tbCode_Validating);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Град:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Име:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Кратенка:";
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(37, 307);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(158, 23);
+            this.btnConfirm.TabIndex = 6;
+            this.btnConfirm.Text = "Зачувај";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(201, 307);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(158, 23);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "Откажи";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // AddAirport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 370);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbCode);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.tbCity);
+            this.Name = "AddAirport";
+            this.Text = "AddAirport";
+            this.Load += new System.EventHandler(this.AddAirport_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbCity;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbCode;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
---
+++ Airports/AddDestination.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class AddDestination : Form
+    {
+        public Destination CreatedDestination { get; set; }
+        public AddDestination()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            if (ValidateChildren())
+            {
+                CreatedDestination = new Destination(tbDestinationName.Text, (int)nudDistance.Value, (int)nudPrice.Value);
+                this.DialogResult = DialogResult.OK;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
+    }
+}
